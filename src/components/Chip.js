70c87a19041fd575c306fd816984d4eb6bc342ad
Ken Fehling <ken@androidideas.org>
2016-06-21 19:05:17 -0400

    Chip, not horse

--- src/components/Chip.js
+++ src/components/Chip.js
@@ -1,5 +1,9 @@
 import {HOUR} from '../constants/Constants';
 
+export function getFullChipImage(value) {
+  return require(`../../img/chips/${value}.png`);
+}
+
 export default ({value, minute, duration=HOUR}) => (
   <img style={{float: minute === 30 ? 'right' : 'left'}}
   src={require(`../../img/chips/${value + (duration === HOUR ? '' : ('half' + (minute === 30 ? '1' : '2' )))}.png`)} />
