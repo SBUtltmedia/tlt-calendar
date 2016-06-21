923777577a801c35525de26b0663dbf952490046
Ken Fehling <ken@androidideas.org>
2016-06-20 20:41:32 -0400

    Added rest of locations

--- src/constants/Settings.js
+++ src/constants/Settings.js
@@ -1,6 +1,13 @@
-export const RANKS = [1, 2, 3, 4];
+export const RANKS = [ 1, 2, 3, 4 ];
 
 export const LOCATIONS = [
-  {name: 'Main Library'},
-  {name: 'Central Reading Room'}
+  { name: 'Main Library' },
+  { name: 'Central Reading Room' },
+  { name: 'North Reading Room - 1st Floor' },
+  { name: 'North Reading Room - 2nd Floor' },
+  { name: 'Student Union' },
+  { name: 'Engineering' },
+  { name: 'Math/Physics' },
+  { name: 'SBS (6th Floor)' },
+  { name: 'Sports Center' },
 ];
