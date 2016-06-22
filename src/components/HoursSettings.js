8e145002bc690d9d9e681d999d9047510f98ed24
TLT Media Lab <tltmedia@gmail.com>
2016-06-22 19:30:56 -0400

    Spruced up left menu

--- src/components/HoursSettings.js
+++ src/components/HoursSettings.js
@@ -2,12 +2,21 @@ import styles from './HoursSettings.scss';
 
 export default () => (
   <form className={styles.container}>
-    <div>
-      <label for="hours-desired">Weekly hours desired</label>
-      <input id="hours-desired" />
-    </div>
-    <div>
-      Main library required hours: {4}
+    <p>
+      <label for="hours-desired">Weekly hours desired: </label>
+      <input id="hours-desired" className="hours-desired value" value={10} onChange={() => {}} />
+      <span className="star">*</span>
+    </p>
+    <p>
+      Main library required hours: <span className="value">{4}</span>
+    </p>
+    <div className="notes">
+      <p>This value determines your approximate ﬁnal hours.</p>
+      <p>
+        Enter as many preferences as you’d like and your
+        schedule permits. The more availability you provide,
+        the more changes of getting your first choice.
+      </p>
     </div>
   </form>
 )
