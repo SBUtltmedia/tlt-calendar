7a5efe3011faf44c88d938a3e3464ec41f3b8cb5
Ken Fehling <ken@androidideas.org>
2016-06-23 13:43:51 -0400

    Refactored over cell style

--- src/components/CalendarCell.js
+++ src/components/CalendarCell.js
@@ -7,12 +7,6 @@ import _ from 'lodash';
 import { dayMinus1, hourMinus1 } from '../utils/time';
 import styles from './CalendarCell.scss';
 
-function getCellStyle(isOver) {
-  const backgroundColor = isOver ? 'blue' : null;
-  const opacity = isOver ? 0.4 : 1;
-  return {backgroundColor, opacity};
-}
-
 const fullCellTarget = {
   drop(props, monitor) {
     props.placeChip(monitor.getItem().value, props.day, props.hour, 0);
@@ -42,7 +36,7 @@ class FullCell extends Component {
   render() {
     const { isOver, canDrop, connectDropTarget, chipsPlaced, day, hour } = this.props;
     const chips = utils.getChipsInSlot(chipsPlaced, day, hour);
-    return connectDropTarget(<div style={getCellStyle(isOver)}className="full-cell">
+    return connectDropTarget(<div className={`cell full ${isOver ? 'over' : ''}`}>
       { _.map(chips, (chip, i) => <Chip {...chip} key={i} />) }
     </div>);
   }
@@ -63,7 +57,7 @@ class HalfCell extends Component {
   };
   render () {
     const { isOver, canDrop, connectDropTarget, chipsPlaced, side } = this.props;
-    return connectDropTarget(<div style={getCellStyle(isOver)} className={`half-cell ${side}`}></div>);
+    return connectDropTarget(<div className={`cell half ${side} ${isOver ? 'over' : ''}`}></div>);
   }
 }
 
