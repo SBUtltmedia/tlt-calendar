cbffa6b53525ac913719aebed4e4155d081c9bd9
Ken Fehling <ken@androidideas.org>
2016-06-23 18:26:07 -0400

    Formatting

--- src/components/DraggableChip.js
+++ src/components/DraggableChip.js
@@ -4,36 +4,36 @@ import ItemTypes from '../constants/ItemTypes';
 import Chip, { getChipImage } from './Chip';
 
 const boxSource = {
-    beginDrag(props) {
-        return {
-            value: props.value,
-            disabled: props.disabled
-        };
-    }
+  beginDrag(props) {
+    return {
+      value: props.value,
+      disabled: props.disabled
+    };
+  }
 };
 
 @DragSource(ItemTypes.CHIP, boxSource, (connect, monitor) => ({
-    connectDragSource: connect.dragSource(),
-    connectDragPreview: connect.dragPreview(),
-    isDragging: monitor.isDragging()
+  connectDragSource: connect.dragSource(),
+  connectDragPreview: connect.dragPreview(),
+  isDragging: monitor.isDragging()
 }))
 export default class DraggableChip extends Component {
     static propTypes = {
-        disabled: PropTypes.bool,
-        connectDragSource: PropTypes.func.isRequired,
-        connectDragPreview: PropTypes.func.isRequired,
-        isDragging: PropTypes.bool.isRequired,
-        value: PropTypes.number.isRequired
+      disabled: PropTypes.bool,
+      connectDragSource: PropTypes.func.isRequired,
+      connectDragPreview: PropTypes.func.isRequired,
+      isDragging: PropTypes.bool.isRequired,
+      value: PropTypes.number.isRequired
     };
 
     componentDidMount() {
-        const { connectDragPreview, value } = this.props;
-        getChipImage(value, connectDragPreview);
+      const { connectDragPreview, value } = this.props;
+      getChipImage(value, connectDragPreview);
     }
 
     render() {
-        const { connectDragSource, isDragging, disabled, value } = this.props;
-        const opacity = isDragging || disabled ? 0.1 : 1;
-        return connectDragSource(<div style={{opacity}}><Chip value={value} /></div>);
+      const { connectDragSource, isDragging, disabled, value } = this.props;
+      const opacity = isDragging || disabled ? 0.1 : 1;
+      return connectDragSource(<div style={{opacity}}><Chip value={value} /></div>);
     }
 }
