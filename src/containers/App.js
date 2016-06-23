52f9d7f92f5c2bafb4dbf57424310feefef60a4c
TLT Media Lab <tltmedia@gmail.com>
2016-06-22 21:03:57 -0400

    Trying to get react-router working

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,26 +1,8 @@
-import ReactDom  from 'react-dom';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import HourPreferences from './HourPreferences';
 import styles from './App.scss';
 
-class App extends React.Component {
-
-  componentWillMount() {
-    const { dispatch } = this.props;
-    //this.actions = bindActionCreators(Actions, dispatch);
-  }
-
-  render() {
-    const {  } = this.props;
-    return <div className={styles.container}>
-      <HourPreferences />
-    </div>;
-  }
-}
-
-function mapStateToProps(state) {
-  return {...state};
-}
-
-export default connect(mapStateToProps)(App);
+export default (props) => {
+  console.log(props);
+  return <div className={styles.container}>
+    
+  </div>
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import ReactDom  from 'react-dom';
+import { render }  from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './containers/App';
 import configureStore from './store/configureStore';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { HourPreferences } from './containers/HourPreferences';
+import { AdminDashboard } from './containers/AdminDashboard';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 
 const store = configureStore();
+const history = syncHistoryWithStore(browserHistory, store);
 
-ReactDom.render(
+render((
   <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={HourPreferences} />
+        <Route path="student" component={HourPreferences} />
+        <Route path="admin" component={AdminDashboard} />
+      </Route>
+    </Router>
+  </Provider>
+),document.getElementById('root'));
