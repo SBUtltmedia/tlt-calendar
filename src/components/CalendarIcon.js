9a7a4e8c49b82be750fc15a0d777e13e911ec823
Kenneth T Fehling <kfehling@dh017-163.medilab.tlt.stonybrook.edu>
2016-06-29 07:59:45 -0400

    Setting up info box, style refactoring

--- src/components/CalendarIcon.js
+++ src/components/CalendarIcon.js
@@ -2,6 +2,8 @@ import { HOUR } from '../constants/Constants';
 import { Component, PropTypes } from 'react';
 import { DragSource } from 'react-dnd';
 import _ from 'lodash';
+import styles from './CalendarIcon.scss';
+
 const FORMAT = 'png';
 const req = require.context('img', true, /^\.\/.*\.png$/);
 
@@ -54,7 +56,7 @@ export default class CalendarIcon extends Component {
   render() {
     const {minute, disabled, connectDragSource, isDragging, size} = this.props;
     const opacity = isDragging || disabled ? 0.1 : 1;
-    return connectDragSource(<img src={req(this.getFilePath())}
+    return connectDragSource(<img className={styles.icon} src={req(this.getFilePath())}
       style={{opacity, width: size, height: size, float: minute === 30 ? 'right' : 'left'}} />);
   }
 }
--- src/containers/AdminCalendarPage.js
+++ src/containers/AdminCalendarPage.js
@@ -4,6 +4,7 @@ import HTML5Backend from 'react-dnd-html5-backend';
 import AdminCalendarGrid from '../components/admin/AdminCalendarGrid';
 import AdminBank from '../components/admin/AdminBank';
 import AdminCalendarTrash from '../components/admin/AdminCalendarTrash';
+import AdminCalendarInfoBox from '../components/admin/AdminCalendarInfoBox';
 import styles from './AdminCalendarPage.scss';
 
 @DragDropContext(HTML5Backend)
@@ -14,6 +15,7 @@ export default class AdminCalendarPage extends Component {
       <div className="controls">
         <div className="bank"><AdminBank /></div>
         <div className="trash"><AdminCalendarTrash /></div>
+        <div className="info"><AdminCalendarInfoBox /></div>
       </div>
 		</div>;
 	}
