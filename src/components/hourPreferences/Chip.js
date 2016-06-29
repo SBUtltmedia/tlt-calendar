4f036b40a8d608a635397f92b82c3fab0a1e82a8
Kenneth T Fehling <kfehling@dh017-163.medilab.tlt.stonybrook.edu>
2016-06-29 03:05:51 -0400

    Removed need for getChipSize

--- src/components/hourPreferences/Chip.js
+++ src/components/hourPreferences/Chip.js
@@ -1,10 +1,6 @@
 import { HOUR } from '../../constants/Constants';
 import ItemTypes from '../../constants/ItemTypes';
-import CalendarIcon, { getSize, getImage } from '../CalendarIcon';
-
-export function getChipSize(callback) {
-  getSize('chips', 1, callback);
-}
+import CalendarIcon from '../CalendarIcon';
 
 export default props => (
   <CalendarIcon path='chips' file={props.value} itemType={ItemTypes.CHIP} {...props} />
