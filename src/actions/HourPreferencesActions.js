ecb4382b015fcca4ff3b7ef95573539a5f2f23e4
Ken Fehling <ken@androidideas.org>
2016-07-11 19:27:15 -0400

    ok

--- src/actions/HourPreferencesActions.js
+++ src/actions/HourPreferencesActions.js
@@ -1,5 +1,20 @@
-import { PLACE_CHIP, REMOVE_CHIP } from '../constants/ActionTypes';
+import { RECEIVE_PREFERENCES, PLACE_CHIP, REMOVE_CHIP } from '../constants/ActionTypes';
 import _ from 'lodash';
+
+function receivePreferences(json) {
+  return {
+    type: RECEIVE_SCHEDULE,
+    preferences: json
+  }
+}
+
+export function fetchPreferences(location) {
+  return dispatch => {
+    return fetch(`${DATA_PATH}/preferencess/${location}.json`)
+      .then(response => response.json())
+      .then(json => dispatch(receivePreferences(json)))
+  }
+}
 
 export function placeItem(chip) {
   return _.assign({}, chip, {type: PLACE_CHIP});
