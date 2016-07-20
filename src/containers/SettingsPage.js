beece3917e4a6f16366910c20588125b8f3eec3b
Ken Fehling <ken@androidideas.org>
2016-07-20 18:35:26 -0400

    Split employee/admin settings within component

--- src/containers/SettingsPage.js
+++ src/containers/SettingsPage.js
@@ -3,9 +3,14 @@ import EmployeeIcon from '../components/EmployeeIcon';
 import styles from './SettingsPage.scss';
 import { changeSettings } from '../actions/UserActions';
 
-const SettingsPage = ({user}) => (
-  <div className={styles.container}>
-    <h1>Settings</h1>
+const AdminSettings = () => (
+  <div>
+    Admin Settings
+  </div>
+);
+
+const EmployeeSettings = ({user}) => (
+  <div>
     <h3>Icon</h3>
     <EmployeeIcon className="user-icon" employee={user} />
     <p>
@@ -13,6 +18,13 @@ const SettingsPage = ({user}) => (
       <a target="_blank" href="http://gravatar.com">Gravatar</a>
       with your stonybrook.edu email.
     </p>
+  </div>
+);
+
+const SettingsPage = ({user}) => (
+  <div className={styles.container}>
+    <h1>Settings</h1>
+    {user.isAdmin ? <AdminSettings /> : <EmployeeSettings user={user} />}
   </div>
 );
 
