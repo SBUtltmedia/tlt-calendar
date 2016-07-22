21824493fcde1dab127d5b68323e6a5b16905488
Ken Fehling <ken@androidideas.org>
2016-07-22 13:13:08 -0400

    Fixing up settings UI

--- src/containers/SettingsPage.js
+++ src/containers/SettingsPage.js
@@ -11,19 +11,29 @@ const AdminSettings = () => (
 
 const EmployeeSettings = ({user}) => (
   <div>
-    <h3>Icon</h3>
-    <EmployeeIcon className="user-icon" employee={user} />
-    <p>
-      To get a custom icon, use
-      <a target="_blank" href="http://gravatar.com">Gravatar</a>
-      with your stonybrook.edu email.
-    </p>
+    <table>
+      <tbody>
+        <tr>
+          <td>Icon</td>
+          <td>
+            <EmployeeIcon className="user-icon" employee={user} />
+            <div>
+              <div><a type="button" className="btn btn-default" target="_blank" href="http://gravatar.com">Change icon</a></div>
+              <div>
+                To get a custom icon, use <a target="_blank" href="http://gravatar.com">Gravatar</a> with your stonybrook.edu email.
+              </div>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <hr />
   </div>
 );
 
 const SettingsPage = ({user}) => (
   <div className={styles.container}>
-    <h1>Settings</h1>
+    <h2>Settings</h2>
     {user.isAdmin ? <AdminSettings /> : <EmployeeSettings user={user} />}
   </div>
 );
