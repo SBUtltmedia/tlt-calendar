1e1478c79e5d4d83516bbb32f350565b180a8e74
Ken Fehling <ken@androidideas.org>
2016-07-22 12:18:07 -0400

    Fixed employee null

--- src/actions/HourPreferencesActions.js
+++ src/actions/HourPreferencesActions.js
@@ -3,6 +3,7 @@ import { DATA_PATH } from '../constants/Settings';
 import _ from 'lodash';
 
 function receivePreferences(json) {
+  console.log(json);
   return {
     type: RECEIVE_PREFERENCES,
     preferences: json
--- src/utils/employees.js
+++ src/utils/employees.js
@@ -1,6 +1,9 @@
 import _ from 'lodash';
 
 export function markGravatarLoadFailed(employees, employee) {
-  const index = _.findIndex(employees, e => e.id === employee.id);
-  return [...employees.slice(0, index), {...employee, missingGravatar: true}, ...employees.slice(index + 1)]
+  if (employee) {
+    const index = _.findIndex(employees, e => e.id === employee.id);
+    return [...employees.slice(0, index), {...employee, missingGravatar: true}, ...employees.slice(index + 1)]
+  }
+  return employees;
 }
