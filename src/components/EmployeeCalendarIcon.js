44e37ecb21537e83279d912dce728200aa3c7b01
Ken Fehling <ken@androidideas.org>
2016-07-27 00:44:06 -0400

    ok

--- src/components/EmployeeCalendarIcon.js
+++ src/components/EmployeeCalendarIcon.js
@@ -2,10 +2,9 @@ import { HOUR } from '../constants/Constants';
 import CalendarIcon from './CalendarIcon';
 import EmployeeIcon from './EmployeeIcon';
 
-const ViewComponent = ({value}) => {
-  console.log("ViewComponent", value);
-  return <EmployeeIcon employee={value} />
-};
+const ViewComponent = ({value}) => (
+  <EmployeeIcon employee={value} />
+);
 
 export default props => (
   <CalendarIcon {...props} name={props.value.name} description={props.value.name} viewComponent={ViewComponent} />
