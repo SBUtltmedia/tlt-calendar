536ffeda4fe2c60bba199c6cba63b5e660a1120f
Ken Fehling <ken@androidideas.org>
2016-08-22 17:32:05 -0400

    Refactored slot colors

--- src/components/admin/Slot.js
+++ src/components/admin/Slot.js
@@ -2,22 +2,17 @@ import { EIGHT_HOURS, FOUR_HOURS, TWO_HOURS, HOUR, HALF_HOUR } from '../../const
 import { renderToStaticMarkup } from 'react-dom/server';
 import CalendarIcon from '../CalendarIcon';
 import _ from 'lodash';
-
-const colors = {};
-colors[String(EIGHT_HOURS)] = '#F80';
-colors[String(FOUR_HOURS)] = '#F0F';
-colors[String(TWO_HOURS)] = '#06F';
-colors[String(HOUR)] = '#090';
+import styles from './Slot.scss';
 
 const handle = 3;
 
 const ViewComponent = ({duration}) => {
   const key = String(duration);
-  return <svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%' viewBox={`0 0 ${duration} 60`}>
+  return <svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%' viewBox={`0 0 ${duration} 60`} className={`slot_${duration}`}>
     <g>
-      <rect width={handle} height='60' x='0' y='0' style={{fill:colors[key], fillOpacity: 1}} />
-      <rect width={duration - handle * 2} height={60 - handle * 2} x={handle} y={handle} style={{fill:colors[key], fillOpacity: 0.5}} />
-      <rect width={handle} height='60' x={duration - handle} y='0' style={{fill:colors[key], fillOpacity: 1}} />
+      <rect width={handle} height='60' x='0' y='0' />
+      <rect width={duration - handle * 2} height={60 - handle * 2} x={handle} y={handle} style={{fillOpacity: 0.5}} />
+      <rect width={handle} height='60' x={duration - handle} y='0' />
     </g>
   </svg>
 };
