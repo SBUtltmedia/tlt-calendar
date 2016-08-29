bb2e85dbf0e90c6830f4efa97d4c8278bd65a677
Ken Fehling <ken@androidideas.org>
2016-08-29 13:39:38 -0400

    employees list map

--- src/reducers/employees.js
+++ src/reducers/employees.js
@@ -1,11 +1,12 @@
 import { RECEIVE_EMPLOYEES, GRAVATAR_LOAD_FAILED } from '../constants/ActionTypes';
 import { markGravatarLoadFailedInList } from '../utils/employees';
+import _ from 'lodash';
 
 const initialState = [];
 
 export default function employees(state=initialState, action) {
   switch (action.type) {
-    case RECEIVE_EMPLOYEES: return action.employees;
+    case RECEIVE_EMPLOYEES: return _.map(action.employees, e => ({...e, name: e.firstname + ' ' + e.lastname}));
     case GRAVATAR_LOAD_FAILED: return markGravatarLoadFailedInList(state, action.employee);
     default: return state;
   }
