92422b2a2c2f4317aa938919b7a501ef6f32281b
Ken Fehling <ken@androidideas.org>
2016-10-28 12:44:14 -0400

    Fixed data import functions

--- src/utils/api.js
+++ src/utils/api.js
@@ -11,27 +11,34 @@ const FETCH_PARAMS = {
 const getBasePath = () => process.env.NODE_ENV === 'production' ? REMOTE_DATA_PATH : LOCAL_DATA_PATH;
 const getExtension = () => process.env.NODE_ENV === 'production' ? '' : '.json';
 
-const mapReceivedSchedulesOrSlots = json => {
-    let id = 1;
-    return {items: _.map(json, item => ({...item, group: item.location, id: id++}))};
+const mapReceivedTimelineItems = items => {
+  let id = 1;
+  return _.map(items, item => ({...item, id: id++}));
 };
+
+const mapReceivedSchedulesOrSlots = items => {
+    return {items: _.map(mapReceivedTimelineItems(items), item => ({...item, group: item.location}))};
+};
+
+const mapScheduleOrSlotStateToData = state =>
+  _.map(state.timeline.items, item => ({..._.omit(item, ['group']), location: item.group}));
 
 export function getHandler(type) {
   switch (type) {
     case SCHEDULE: return {
       mapStateToPath: () => '/schedules',
-      mapStateToData: state => ({...state.schedule, items: _.values(state.timeline.items)}),
+      mapStateToData: mapScheduleOrSlotStateToData,
       mapReceivedData: mapReceivedSchedulesOrSlots
     };
     case SLOTS: return {
       mapStateToPath: () => '/slots',
-      mapStateToData: state => ({...state.slots, items: _.values(state.timeline.items)}),
+      mapStateToData: mapScheduleOrSlotStateToData,
       mapReceivedData: mapReceivedSchedulesOrSlots
     };
     case HOUR_PREFERENCES: return {
       mapStateToPath: state => `/employees/${state.hourPreferences.employee.netId}/hour-preferences`,
-      mapStateToData: state => ({...state.hourPreferences, items: _.values(state.timeline.items)}),
-      mapReceivedData: _.identity
+      mapStateToData: state => ({...state.hourPreferences, items: _.map(state.timeline.items, item => _.omit(item, ['group']))}),
+      mapReceivedData: json => ({...json, items: mapReceivedTimelineItems(json.items)})
     }
   }
 }
