5eef604eb8f1e0f4544bc328d1bee3c5caac8f95
Ken Fehling <ken@androidideas.org>
2016-11-28 15:44:18 -0500

    Got JSON to CSV working

--- src/containers/SpreadsheetDashboard.js
+++ src/containers/SpreadsheetDashboard.js
@@ -1,18 +1,18 @@
 import styles from './SpreadsheetDashboard.scss';
 import json2csv from 'json2csv';
-import DownloadButton from 'downloadbutton';
+import fileDownload from 'react-file-download';
 
-function fakeCsv(callback) {
+function fakeCsv() {
   const data = {a:1, b:2};
-  json2csv({
-      data,
-      fields: ['a', 'b']
-  }, callback);
+  return json2csv({
+    data,
+    fields: ['a', 'b']
+  });
 }
 
 export default () => (
   <div className={styles.container}>
-    <DownloadButton genFile={f => fakeCsv(f)} async={true} className="btn btn-success">Download spreadsheet</DownloadButton>
-    <DownloadButton className="btn btn-success">Upload spreadsheet</DownloadButton>
+    <button onClick={() => fileDownload(fakeCsv(), 'filename.csv')} className="btn btn-success">Download spreadsheet</button>
+    <button className="btn btn-success">Upload spreadsheet</button>
   </div>
 );
