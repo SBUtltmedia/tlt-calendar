ad17b105e9138eca2a6d2314c3538137d2e23c5c
Ken Fehling <ken@androidideas.org>
2017-03-30 16:56:11 -0400

    Not yet...

--- src/components/HourPreferencesGrid.js
+++ src/components/HourPreferencesGrid.js
@@ -1,52 +1,63 @@
-import { PropTypes } from 'react';
-import _ from 'lodash';
-import styles from './HourPreferencesGrid.scss';
-import HourPreferencesGridCell from './HourPreferencesGridCell';
-import { getHourLabel, timeToIndex } from '../utils/time';
-import { SHORT_DAYS } from '../constants/Settings';
+import { PropTypes } from 'react'
+import _ from 'lodash'
+import styles from './HourPreferencesGrid.scss'
+import HourPreferencesGridCell from './HourPreferencesGridCell'
+import { getHourLabel, timeToIndex } from '../utils/time'
+import { SHORT_DAYS } from '../constants/Settings'
+import ReactTooltip from 'react-tooltip'
 
-const HourPreferencesGrid = ({items}) => (
+const Hints = ({numFilledIn, children}) => (
+  numFilledIn === 0 ?
+    <ReactTooltip place="top" type="light" effect="float">
+      {children}
+    </ReactTooltip> : {children}
+)
+
+const HourPreferencesGrid = ({items, numFilledIn}) => (
   <div className={styles.container}>
-    <table className='calendar'>
-      <thead>
-        <tr>
-          { _.map(_.range(26), i => (
-            <td key={i} colSpan='2'>
-              <div className='hour-label'>
-                {i === 0 ? '' : getHourLabel(i - 1)}
-              </div>
-            </td>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        { _.map(SHORT_DAYS, (day, i) => (
-          <tr key={i}>
-            <td className='day-label' colSpan='2'>{day}</td>
-            { _.map(_.range(48), col => (
-              <td key={col}>
-                <HourPreferencesGridCell
-                  items={items}
-                  index={timeToIndex({
-                    day: i,
-                    hour: Math.floor(col / 2),
-                    minute: col % 2 === 0 ? 0 : 30
-                  })}
-                />
+    <Hints numFilledIn={numFilledIn}>
+      <table className='calendar'>
+        <thead>
+          <tr>
+            { _.map(_.range(26), i => (
+              <td key={i} colSpan='2'>
+                <div className='hour-label'>
+                  {i === 0 ? '' : getHourLabel(i - 1)}
+                </div>
               </td>
-            )) }
-            <td className='day-label' colSpan='2'>
-              {day}
-            </td>
+            ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          { _.map(SHORT_DAYS, (day, i) => (
+            <tr key={i}>
+              <td className='day-label' colSpan='2'>{day}</td>
+              { _.map(_.range(48), col => (
+                <td key={col}>
+                  <HourPreferencesGridCell
+                    items={items}
+                    index={timeToIndex({
+                      day: i,
+                      hour: Math.floor(col / 2),
+                      minute: col % 2 === 0 ? 0 : 30
+                    })}
+                  />
+                </td>
+              )) }
+              <td className='day-label' colSpan='2'>
+                {day}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Hints>
   </div>
 )
 
 HourPreferencesGrid.propTypes = {
-  items: PropTypes.arrayOf(PropTypes.number).isRequired
+  items: PropTypes.arrayOf(PropTypes.number).isRequired,
+  numFilledIn: _.sumBy(items, x => x > 0)
 }
 
 export default HourPreferencesGrid
